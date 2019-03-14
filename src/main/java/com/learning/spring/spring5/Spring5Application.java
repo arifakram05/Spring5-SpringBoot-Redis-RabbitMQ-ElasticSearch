45224883af98d92b5e7b29b6e4435f70d95b03e4
Arif Mohammed <arif.mohammed@cerner.com>
2019-03-14 10:10:42 -0500

    Changes to make Spring Boot app work with external Tomcat

--- src/main/java/com/learning/spring/spring5/Spring5Application.java
+++ src/main/java/com/learning/spring/spring5/Spring5Application.java
@@ -2,10 +2,17 @@ package com.learning.spring.spring5;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 // behind the scenes, @SpringBootApplication is equivalent to @Configuration, @EnableAutoConfiguration, and @ComponentScan together
 @SpringBootApplication
-public class Spring5Application {
+public class Spring5Application extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Spring5Application.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Spring5Application.class, args);
